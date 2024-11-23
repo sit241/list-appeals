bccd072901a64ea51d7b3ca93346704473dd3c08
Петя Sit <petia.cyrnicov@gmail.com>
2024-11-23 10:43:08 +0300

    save

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,36 +1,35 @@
 <template>
-    <div>
-      <p>Count: {{ count }}</p>
-      <p>Double Count: {{ doubleCount }}</p>
-      <button @click="increment">Increment</button>
-      <button @click="decrement">Decrement</button>
-      <button @click="incrementAsync">Increment Async</button>
-    </div>
-  </template>
-  
-  <script>
+  <div>
+    <p>Count: {{ count }}</p>
+    <p>Double Count: {{ doubleCount }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <button @click="incrementAsync">Increment Async</button>
+  </div>
+</template>
+
+<script>
   export default {
     computed: {
       // Доступ к состоянию через геттеры
       count() {
-        return this.$store.state.count
+        return this.$store.state.count;
       },
       doubleCount() {
-        return this.$store.getters.doubleCount
-      }
+        return this.$store.getters.doubleCount;
+      },
     },
     methods: {
       // Вызов мутаций и действий
       increment() {
-        this.$store.commit('increment')
+        this.$store.commit('increment');
       },
       decrement() {
-        this.$store.commit('decrement')
+        this.$store.commit('decrement');
       },
       incrementAsync() {
-        this.$store.dispatch('incrementAsync')
-      }
-    }
-  }
-  </script>
-  
+        this.$store.dispatch('incrementAsync');
+      },
+    },
+  };
+</script>
