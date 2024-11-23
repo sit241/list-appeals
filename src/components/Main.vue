a2e6de0fc57aaf2961a6aafb8207d943e0fe543a
Петя Sit <petia.cyrnicov@gmail.com>
2024-11-23 10:27:10 +0300

    почистил от базового мусора

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<script>
+
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+  };
+</script>
+
+<template>
+  <div class="main">
+    Main
+  </div>
+</template>
