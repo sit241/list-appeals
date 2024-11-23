c91891d47bd12ea3bc22f22fba71d1f4ebacf8e1
Петя Sit <petia.cyrnicov@gmail.com>
2024-11-23 14:48:11 +0300

    Добавил кнопку выхода

---
+++ src/components/other/LogoutButton.vue
@@ -0,0 +1,41 @@
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'LogoutButton',
+    methods: {
+      ...mapActions('auth', ['logout']),
+
+      async handleLogout() {
+        try {
+          await this.logout();
+
+          this.$router.push('/login');
+        } catch (error) {
+          console.error('Ошибка при выходе:', error);
+        }
+      },
+    },
+  };
+</script>
+
+<template>
+  <button @click="handleLogout" class="logout-button">
+    Выйти
+  </button>
+</template>
+
+<style scoped>
+  .logout-button {
+    padding: 10px 20px;
+    background-color: #ff4d4f;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .logout-button:hover {
+    background-color: #ff7875;
+  }
+</style>
--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,15 +1,30 @@
 <script>
   import AppealsList from '@/components/table/AppealsList.vue';
+  import LogoutButton from '@/components/other/LogoutButton.vue';
 
   export default {
     components: {
       AppealsList,
+      LogoutButton,
     },
   };
 </script>
 
 <template>
   <div>
+    <div class="header">
+      <LogoutButton />
+    </div>
+
     <AppealsList />
   </div>
 </template>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: flex-end;
+
+    width: 100%;
+  }
+</style>
