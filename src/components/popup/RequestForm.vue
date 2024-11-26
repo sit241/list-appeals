37e55c87cba497add9db6275a33c4239417fd2f8
Петя Sit <petia.cyrnicov@gmail.com>
2024-11-26 10:45:31 +0300

    стилизовал попап

--- src/components/popup/RequestForm.vue
+++ src/components/popup/RequestForm.vue
@@ -99,131 +99,210 @@
 </script>
 
 <template>
-  <div class="appeal-form">
-    <div class="test" @click="test">test</div>
-
-    <div v-if="isLoading" class="loading">Загрузка...</div>
-    <div v-if="error" class="error">{{ error }}</div>
-
-    <form @submit.prevent="handleSubmit">
-      <div class="select">
-        <CustomSelect
-          :options="premises"
-          v-model="houseId"
-          placeholder="Дом"
-          @input="selectPremise"
-        />
+  <div class="back">
+    <div class="form-wrapper">
+      <div class="form-header">
+        <h3>Создание заявки</h3>
+        <span>Новая</span>
       </div>
-
-      <div class="select">
-        <CustomSelect
-          :options="apartments"
-          v-model="HouseApartmentId"
-          placeholder="Квартира"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="datetime">Дата и время</label>
-        <input
-          type="datetime-local"
-          id="datetime"
-          v-model="computedAppealData.dateTime"
-          placeholder="Выберите дату и время"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="LastName">Фамилия</label>
+      <form @submit.prevent="handleSubmit">
+        <div class="form-row">
+          <div class="wrapper-select">
+            <CustomSelect
+              :options="premises"
+              v-model="houseId"
+              placeholder="Дом"
+              @input="selectPremise"
+            />
+          </div>
+          <div class="wrapper-select">
+            <CustomSelect
+              :options="apartments"
+              v-model="HouseApartmentId"
+              placeholder="Квартира"
+            />
+          </div>
+          <input
+            type="datetime-local"
+            v-model="computedAppealData.dateTime"
+            placeholder="Срок"
+            class="input-field"
+          />
+        </div>
+        <div class="form-row">
+          <input
+            type="text"
+            v-model="computedAppealData.lastName"
+            placeholder="Фамилия"
+            class="input-field"
+          />
+          <input
+            type="text"
+            v-model="computedAppealData.firstName"
+            placeholder="Имя"
+            class="input-field"
+          />
+          <input
+            type="text"
+            v-model="computedAppealData.middleName"
+            placeholder="Отчество"
+            class="input-field"
+          />
+          <input
+            type="tel"
+            v-model="computedAppealData.phone"
+            placeholder="Телефон"
+            class="input-field"
+          />
+        </div>
         <textarea
-          id="LastName"
-          v-model="computedAppealData.lastName"
-          placeholder="Фамилия"
-        ></textarea>
-      </div>
-
-      <div class="form-group">
-        <label for="FirstName">Имя</label>
-        <textarea
-          id="FirstName"
-          v-model="computedAppealData.firstName"
-          placeholder="Имя"
-        ></textarea>
-      </div>
-
-      <div class="form-group">
-        <label for="MiddleName">Отчество</label>
-        <textarea
-          id="MiddleName"
-          v-model="computedAppealData.middleName"
-          placeholder="Отчество"
-        ></textarea>
-      </div>
-
-      <div class="form-group">
-        <label for="phone">Телефон</label>
-        <input
-          type="tel"
-          id="phone"
-          v-model="computedAppealData.phone"
-          placeholder="Телефон"
-          pattern="^\+?[0-9\s\-]{7,15}$"
-          title="Введите корректный номер телефона в формате +79991234567"
-          required
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="description">Описание</label>
-        <textarea
-          id="description"
           v-model="computedAppealData.description"
-          placeholder="Введите описание"
+          placeholder="Описание заявки"
+          class="textarea-field"
         ></textarea>
-      </div>
 
-      <button type="submit">Сохранить</button>
-    </form>
+        <div class="wrapper-button">
+          <button type="submit" class="submit-button">Создать</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
-<style>
-  .loading {
-    color: blue;
-  }
-  .error {
-    color: red;
-  }
-  .form-group {
-    margin-bottom: 15px;
-  }
-  .dropdown {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    border: 1px solid #ccc;
-  }
-  .dropdown li {
-    padding: 5px;
-    cursor: pointer;
-  }
-  .dropdown li:hover {
-    background-color: #f0f0f0;
+<style lang="scss">
+  @import '@/assets/scss/variables.scss';
+
+  .back {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    z-index: 99;
+    background: rgba(0, 0, 0, 0.2);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
-  input[type='datetime-local'] {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: none;
-    border-radius: 4px;
+  .form-wrapper {
+    background-color: $background-color;
+    padding: 24px 32px;
+    border-radius: 8px;
+    box-shadow: $box-shadow-light;
+    margin: 0 auto;
+
     box-sizing: border-box;
-    background-color: transparent;
-    color: #333;
-    outline: none;
-  }
+    width: 738px;
+    height: 480px;
+    top: 176px;
+    left: 271px;
+    padding: 24px 32px 24px 32px;
+    gap: 24px;
+    border-radius: 8px;
+    opacity: 0px;
 
-  input[type='datetime-local']::placeholder {
-    color: #aaa;
+    .form-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 24px;
+
+      height: 40px;
+
+      h3 {
+        margin: 0;
+        font-size: 18px;
+        color: $text-color;
+      }
+
+      span {
+        font-size: 14px;
+        color: $icon-color;
+      }
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+    }
+
+    .form-row {
+      display: flex;
+      gap: 15px;
+
+      .wrapper-select {
+        width: 215px;
+        height: 56px;
+        border-bottom: 1px solid $border-color;
+        color: $text-color;
+      }
+
+      .input-field {
+        all: unset;
+        flex: 1;
+        padding: 17px 0;
+        border: none;
+        border-bottom: 1px solid $border-color;
+        font-size: 14px;
+        color: $text-color;
+        box-sizing: border-box;
+        width: 157px;
+        height: 56px;
+        text-align: start;
+      }
+    }
+
+    .textarea-field {
+      all: unset;
+      text-align: start;
+
+      width: 100%;
+      height: 132px;
+
+      border: none;
+      border-bottom: 1px solid $border-color;
+      font-size: 14px;
+      color: $text-color;
+
+      font-size: 14px;
+      color: $text-color;
+      resize: none;
+      box-sizing: border-box;
+    }
+
+    .wrapper-button {
+      display: flex;
+      justify-content: end;
+
+      width: 100%;
+      margin-top: -8px;
+
+      .submit-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background-color: $primary-color;
+        border: none;
+        color: #fff;
+        font-size: 16px;
+        cursor: pointer;
+        margin-right: 0;
+
+        width: 87px;
+        height: 36px;
+        padding: 12px 16px 12px 16px;
+        border-radius: 2px;
+
+        &:hover {
+          background-color: $primary-hover;
+        }
+
+        &:focus {
+          outline: none;
+        }
+      }
+    }
   }
 </style>
